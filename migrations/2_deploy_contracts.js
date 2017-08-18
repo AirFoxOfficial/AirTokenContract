4dc853e238801868efa7ee5d025f40d896258e1e
seibelj <james@airfox.io>
2017-08-18 14:23:07 -0400

    Removed old contract code

--- migrations/2_deploy_contracts.js
+++
@@ -1,8 +0,0 @@
-var AirToken = artifacts.require("./AirToken.sol");
-
-module.exports = function(deployer, network, accounts) {
-    // Deploy AirToken contract, first account for receviing eth,
-    // second for the AT fund
-    console.log("Accounts", accounts);
-    deployer.deploy(AirToken, accounts[0], accounts[1], 0, 100000);
-};
